5aae521a4591f8f2d2ac49300b07e8608aea592d
Cafuelatte <Phosphorus0102+github@gmail.com>
2026-08-06 02:29:56 +0000

    add main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'ログイン成功！BOT名: {bot.user}')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('Pong!')
+
+TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+bot.run(TOKEN)
